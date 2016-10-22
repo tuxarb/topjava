934464885af0f20adf66794acaa4433989fd6de3
tuxarb <tuxarb777@yandex.ru>
2016-10-22 23:42:49 +0300

    hw07 add hamcrest-all

---
+++ src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
@@ -0,0 +1,65 @@
+package ru.javawebinar.topjava.web;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.util.Profiles;
+
+import javax.annotation.PostConstruct;
+
+@ContextConfiguration({
+        "classpath:spring/spring-mvc",
+        "classpath:spring/spring-app",
+        "classpath:spring/spring-db"
+})
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles({Profiles.ACTIVE_REPOSITORY, Profiles.ACTIVE_DB})
+@Transactional
+public abstract class AbstractControllerTest {
+    private static CharacterEncodingFilter filter = new CharacterEncodingFilter();
+
+    static{
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+    }
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected UserService userService;
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    @PostConstruct
+    public void postConstruct()
+    {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webContext)
+                //.addFilter(filter)
+                .build();
+    }
+
+    @Before
+    public void setUp()
+    {
+        userService.evictCache();
+    }
+
+
+}
---
+++ src/test/java/ru/javawebinar/topjava/web/user/RootControllerTest.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.web.user;
+
+
+import org.junit.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.UserTestData.USER;
+
+public class RootControllerTest extends AbstractControllerTest{
+    @Test
+    public void testUsers() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(1)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
