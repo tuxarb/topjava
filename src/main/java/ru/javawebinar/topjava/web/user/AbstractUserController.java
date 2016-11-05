89dd336a4bd9909d3dfa4263d9b8454ac9b7dcbc
tuxarb <tuxarb777@yandex.ru>
2016-11-05 12:58:10 +0300

    lesson 9 patch 4

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -48,4 +48,8 @@ public abstract class AbstractUserController {
         log.info("getByEmail " + email);
         return service.getByEmail(email);
     }
+
+    public void enable(int id, boolean enabled) {
+        service.enable(id, enabled);
+    }
 }
